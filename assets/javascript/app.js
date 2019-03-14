85a7150ddf17c2aab3b5136989490240461c89b0
J-Falconetti <joseph.falconetti@gmail.com>
2019-03-13 23:05:26 -0400

    stated on JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,17 @@
-// information for google's firebox
 
+// Set Variables
+var shipName = "";
+var shipDestination = "";
+var shipTime = "";
+var shipFrequency = "";
+var nextArrival = "";
+var minutesAway = "";
+var starship = $("#ship-name");
+var starshipDestination = $("#ship-destination");
+var starshipTime = $("#ship-time").mask("00:00");
+var starTimeFreq = $("#time-freq").mask("00");
+
+// Initialize Firebase
   var config = {
     apiKey: "AIzaSyAsv17YRZ4fAKoYe7AUQYCet56jWO4bEVo",
     authDomain: "startravel-cdcf3.firebaseapp.com",
@@ -8,10 +20,5 @@
     storageBucket: "startravel-cdcf3.appspot.com",
     messagingSenderId: "12355858482"
   };
-  firebase.initializeApp(config);
 
-// varibles
-var shipname ""
-var dest ""
-var deptime ""
-var traveltime " "
+firebase.initializeApp(config);
