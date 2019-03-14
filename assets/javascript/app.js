46b4bc000add770e73a6843b972bce89a09bda23
J-Falconetti <joseph.falconetti@gmail.com>
2019-03-13 22:54:18 -0400

    fixed HTML error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,17 @@
-// this is the Javascript page
+// information for google's firebox
+
+  var config = {
+    apiKey: "AIzaSyAsv17YRZ4fAKoYe7AUQYCet56jWO4bEVo",
+    authDomain: "startravel-cdcf3.firebaseapp.com",
+    databaseURL: "https://startravel-cdcf3.firebaseio.com",
+    projectId: "startravel-cdcf3",
+    storageBucket: "startravel-cdcf3.appspot.com",
+    messagingSenderId: "12355858482"
+  };
+  firebase.initializeApp(config);
+
+// varibles
+var shipname ""
+var dest ""
+var deptime ""
+var traveltime " "
